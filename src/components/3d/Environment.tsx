5d438c57207abf7e9b6bd657324d8a8114dcff8c
LouisAugustine <nzlouis.com@gmail.com>
2025-12-20 10:18:38 +1300

    feat: Add a dynamic 3D environment component with day/night cycle.

--- src/components/3d/Environment.tsx
+++ src/components/3d/Environment.tsx
@@ -1,4 +1,4 @@
-import { Sky, Environment as DreiEnvironment } from '@react-three/drei';
+import { Sky } from '@react-three/drei';
 import { useStore } from '../../hooks/useStore';
 
 export const Environment = () => {
@@ -8,15 +8,12 @@ export const Environment = () => {
   const nightSunPosition: [number, number, number] = [10, -5, 10];
 
   return (
-    <>
-      <Sky
-        sunPosition={isNight ? nightSunPosition : daySunPosition}
-        turbidity={isNight ? 10 : 8}
-        rayleigh={isNight ? 0.5 : 6}
-        mieCoefficient={0.005}
-        mieDirectionalG={0.8}
-      />
-      <DreiEnvironment preset={isNight ? 'night' : 'sunset'} />
-    </>
+    <Sky
+      sunPosition={isNight ? nightSunPosition : daySunPosition}
+      turbidity={isNight ? 10 : 8}
+      rayleigh={isNight ? 0.5 : 6}
+      mieCoefficient={0.005}
+      mieDirectionalG={0.8}
+    />
   );
 };
